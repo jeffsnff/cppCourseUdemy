fec4842c7bb01287080358dd68383485bd837bd6
Jeffrey Seneff <jeffsnff@gmail.com>
2022-12-17 07:18:24 -0500

    learning datatypes

--- hello.cpp
+++ hello.cpp
@@ -2,11 +2,13 @@
 
 int main(){
 
-	int a = 5;
-	int b = a + 3;
-	int c = a + b;
+	int number = 4; // integer. Whole numbers positive or negative
+	double typeOfFloat = 2.3; // decimal numbers
+	bool trueOrFalse = true; // boolean. True or false. comes out as 0 for false, 1 for true.
+	std::string user_name = "Jeff"; // used for a string of characters must use "" double qoutes.
+	char grade = 'A'; // Character. Can use '' single qoutes
 
-	std::cout << c << std::endl;
+	std::cout << grade << std::endl;
 	return 0;
 
 }
